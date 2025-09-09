eea6db4f4f4fe136d6546ad2633174d69f3d1979
tallesaugusto <tallesaugusto1989@gmail.com>
2025-09-09 18:29:16 -0300

    initial work

--- src/sensor_manager.cpp
+++ src/sensor_manager.cpp
@@ -14,7 +14,7 @@ void setForcedMode(Bme68x& bme) {
 
 void setForcedModeHeat(Bme68x& bme) {
     bme.setTPH(BME68X_OS_4X, BME68X_OS_2X, BME68X_OS_16X); // T,H,P oversampling
-    bme.setHeaterProf(300, 400); // heater temp 300°C, 100ms
+    bme.setHeaterProf(150, 400); // heater temp 300°C, 100ms
     bme.setOpMode(BME68X_FORCED_MODE);
 }
 
@@ -39,7 +39,7 @@ void setParallelMode(Bme68x& bme) {
 
 // Parallel mode: multiple steps heater profile
 void setParallelModeHP354(Bme68x& bme) {
-    static uint16_t tempProf[] = {320, 100, 100, 100, 200, 200, 200, 320, 320, 320};
+    static uint16_t tempProf[] = {320, 120, 120, 120, 200, 200, 200, 320, 320, 320};
     static uint16_t durProf[]  = {5, 2, 10, 30, 5, 5, 5, 5, 5, 5};
     // static uint16_t durProf[]  = {700, 280, 1400, 4200, 700, 700, 700, 700, 700, 700};
     bme.setTPH(BME68X_OS_4X, BME68X_OS_2X, BME68X_OS_16X);
